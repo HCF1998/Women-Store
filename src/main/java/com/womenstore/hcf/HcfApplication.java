0f2fd5d599486ef2eb6d2d53574795e379008226
hechangfeng <515018620@qq.com>
2020-10-31 10:53:23 +0800

    1.Build project

---
+++ src/main/java/com/womenstore/hcf/HcfApplication.java
@@ -0,0 +1,13 @@
+package com.womenstore.hcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/womenstore/hcf/HcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.womenstore.hcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
