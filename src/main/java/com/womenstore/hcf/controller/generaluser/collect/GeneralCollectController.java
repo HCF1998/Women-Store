40e7b0934e44d2cf29436c079701893d1e318b35
hechangfeng <515018620@qq.com>
2020-11-15 23:39:42 +0800

    1.收藏接口新增添加，遍历方法

--- src/main/java/com/womenstore/hcf/controller/generaluser/collect/GeneralCollectController.java
+++ src/main/java/com/womenstore/hcf/controller/generaluser/collect/GeneralCollectController.java
@@ -2,6 +2,7 @@ package com.womenstore.hcf.controller.generaluser.collect;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.womenstore.hcf.dao.collect.CollectMapper;
 import com.womenstore.hcf.dao.user.UserMapper;
 import com.womenstore.hcf.entity.collect.Collect;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * 用户商品收藏类
@@ -44,9 +46,26 @@ public class GeneralCollectController {
         return new Result(ResultCode.SUCCESS,"收藏商品成功");
     }
 
+    /**
+     * 查询所有商品收藏
+     * @param userId
+     * @return
+     */
     @GetMapping("/findAllCollect/{userId}")
     public Result findAllCollect(@PathVariable("userId")Integer userId){
         log.info("userId:[{}]",userId);
+        QueryWrapper qwCollect = new QueryWrapper();
+        qwCollect.eq("user_Id",userId);
+        List<Collect> collectList = collectMapper.selectList(qwCollect);
+        if (collectList!=null){
+            return new Result(ResultCode.SUCCESS,collectList);
+        }else {
+            return new Result(ResultCode.SUCCESS,"还未收藏任何商品");
+        }
+    }
+
+    @GetMapping("deleteCollect/{collectId}")
+    public Result deleteCollect(@PathVariable("collectId")Integer collectId){
         return null;
     }
 
