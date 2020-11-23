98d9be51180089c3a385e093a247797b03c71a58
hechangfeng <515018620@qq.com>
2020-11-23 17:01:15 +0800

    1.重构用户遍历、用户账号状态修改接口

--- src/main/java/com/womenstore/hcf/controller/admin/user/AdminUserController.java
+++ src/main/java/com/womenstore/hcf/controller/admin/user/AdminUserController.java
@@ -3,7 +3,6 @@ package com.womenstore.hcf.controller.admin.user;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.womenstore.hcf.dao.user.UserMapper;
-import com.womenstore.hcf.entity.product.Product;
 import com.womenstore.hcf.entity.user.User;
 import com.womenstore.hcf.service.impl.UserServiceImpl;
 import com.womenstore.hcf.util.Result;
@@ -22,101 +21,82 @@ import java.util.List;
 @Slf4j
 public class AdminUserController {
 
-    @Autowired
-    private UserMapper userMapper;
+  @Autowired private UserMapper userMapper;
 
-    @Autowired
-    private UserServiceImpl userServiceImpl;
+  @Autowired private UserServiceImpl userServiceImpl;
 
-    /**
-     * 遍历用户
-     * @return
-     */
-    @ApiOperation("遍历用户")
-    @GetMapping("/findAll")
-    public Result finfAll(){
-        List<User> userList = userServiceImpl.list();
-        log.info("userList:{}",userList);
-        return new Result(userList);
+  /**
+   * 遍历用户
+   *
+   * @return 用户列表
+   */
+  @ApiOperation("遍历用户")
+  @GetMapping("/findAll")
+  public Result finfAll() {
+    List<User> userList = userServiceImpl.list();
+    log.info("userList:{}", userList);
+    return new Result(userList);
+  }
+
+  /**
+   * 用户权限修改
+   *
+   * @param jsonObject
+   * @return
+   */
+  @ApiOperation("修改用户权限")
+  @PostMapping("/changeUserPriority")
+  public Result changeUserPriority(@RequestBody JSONObject jsonObject) {
+    log.info("jsonObject:{}", jsonObject);
+    String changPriorityId = (String) jsonObject.get("userId");
+    Integer changPriorityValue = (Integer) jsonObject.get("priorityValue");
+    User changPriorityUser = userServiceImpl.getById(changPriorityId);
+    changPriorityUser.setUserPriority(changPriorityValue);
+    userServiceImpl.updateById(changPriorityUser);
+    if (changPriorityValue == 1) {
+      return new Result(200, "修改用户:" + changPriorityId + "权限为管理员", null);
+    } else if (changPriorityValue == 0) {
+      return new Result(200, "修改用户:" + changPriorityId + "权限为普通用户", null);
     }
+    return new Result(null);
+  }
 
-    /**
-     * 用户权限修改
-     * @param jsonObject
-     * @return
-     */
-    @ApiOperation("修改用户权限")
-    @PostMapping("/changeUserPriority")
-    public Result changeUserPriority(@RequestBody JSONObject jsonObject) {
-        log.info("jsonObject:{}", jsonObject);
-        String changPriorityId = (String) jsonObject.get("userId");
-        Integer changPriorityValue = (Integer) jsonObject.get("priorityValue");
-//        User changPriorityUser = userMapper.selectById(changPriorityId);
-        User changPriorityUser = userServiceImpl.getById(changPriorityId);
-        changPriorityUser.setUserPriority(changPriorityValue);
-        userServiceImpl.updateById(changPriorityUser);
-        if (changPriorityValue == 1) {
-            return new Result(200, "修改用户:" + changPriorityId + "权限为管理员", null);
-        } else if (changPriorityValue == 0) {
-            return new Result(200, "修改用户:" + changPriorityId + "权限为普通用户", null);
-        }
-        return new Result(null);
-    }
+  /**
+   * 删除用户
+   * @param deleteUserId
+   * @return
+   */
+  @ApiOperation("删除用户")
+  @GetMapping("/deleteUser/{deleteUserId}")
+  public Result deleteUser(
+      @RequestBody @PathVariable(value = "deleteUserId") Integer deleteUserId) {
+    log.info("deleteUserId:{}", deleteUserId);
+    userServiceImpl.removeById(deleteUserId);
+    return new Result(200, "删除用户id：[" + deleteUserId + "]成功", null);
+  }
 
-    /**
-     * 删除用户
-     * @param jsonObject
-     * @return
-     */
-    @ApiOperation("删除用户")
-    @GetMapping("/deleteUser")
-    public Result deleteUser(@RequestBody JSONObject jsonObject) {
-        log.info("jsonObject:{}", jsonObject);
-        String deletedId = (String) jsonObject.get("userId");
-        userMapper.deleteById(deletedId);
-        return new Result(200, "删除用户id：" + deletedId + "成功", null);
+  /**
+   * 设置用户账号状态
+   *
+   * @param jsonObject
+   * @return
+   */
+  @ApiOperation("设置用户账号状态")
+  @PostMapping("/banOrUnbanUser")
+  private Result banOrUnbanUser(@RequestBody JSONObject jsonObject) {
+    log.info("jsonObject:[{}]", jsonObject);
+    Integer userId = (Integer) jsonObject.get("userId");
+    Integer status = (Integer) jsonObject.get("status");
+    QueryWrapper queryWrapper = new QueryWrapper();
+    queryWrapper.eq("user_Id", userId);
+    User user = userServiceImpl.getOne(queryWrapper);
+    user.setUserStatus(status);
+    userServiceImpl.updateById(user);
+    if (status == 0) {
+      return new Result(ResultCode.SUCCESS, "成功封禁账号：" + user.getUserAcount());
+    } else if (status == 1) {
+      return new Result(ResultCode.SUCCESS, "成功解禁账号：" + user.getUserAcount());
     }
-
-    /**
-     * 封禁用户
-     * @param jsonObject
-     * @return
-     */
-    @ApiOperation("封禁用户")
-    @GetMapping("/banUsers")
-    public Result banUsers(@RequestBody JSONObject jsonObject) {
-        log.info("jsonObject:[{}]", jsonObject.toJSONString());
-        getBanUser(jsonObject);
-        return new Result(ResultCode.SUCCESS,
-                "成功封禁用户Id:"+jsonObject.get("userAcount"));
-    }
-
-    /**
-     * 解禁用户
-     * @param jsonObject
-     * @return
-     */
-    @ApiOperation("解禁用户")
-    @GetMapping("/unbanUsers")
-    public Result unbanUsers(@RequestBody JSONObject jsonObject){
-        log.info("jsonObject:[{}]", jsonObject.toJSONString());
-        getBanUser(jsonObject);
-        return new Result(ResultCode.SUCCESS,
-                "成功解禁用户Id:"+jsonObject.get("userAcount"));
-    }
-
-    private void getBanUser(JSONObject jsonObject) {
-        String userId = (String) jsonObject.get("userAcount");
-        QueryWrapper queryWrapper = new QueryWrapper();
-        queryWrapper.eq("user_Acount",userId);
-        User user  = userMapper.selectOne(queryWrapper);
-        Integer status =(Integer)jsonObject.get("userStatus");
-        if (status==1){
-            user.setUserStatus(1);
-            userMapper.updateById(user);
-        }else if (status==0){
-            user.setUserStatus(0);
-            userMapper.updateById(user);
-        }
-    }
+    return null;
+  }
 }
